28b60cbc6b85a0f3d837fd17b71ddfcbec8bed83
hrprem16 <hrprem16@gmail.com>
2024-01-19 15:15:28 +0530

    [Prince] Added: Snake and Ladder start with pos zero

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main()
+        {
+            int position = 0;
+            Console.WriteLine($"Player Postion is {position}");
+        }
+    }
+}
