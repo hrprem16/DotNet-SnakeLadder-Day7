2ab129d306a7361e186dbfe3b8d1f1a6da5ae771
hrprem16 <hrprem16@gmail.com>
2024-01-19 16:29:27 +0530

    [Prince] Added: uc4

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,18 +5,23 @@ class SnakeLadder
     class Program
     {
         static void Main()
+        { 
+         // Initialize random number generator
+        Random random = new Random();
+
+        // Player's initial position
+        int playerPosition = 0;
+
+        // Winning position
+        int winningPosition = 100;
+
+        while (playerPosition<winningPosition)
         {
-            // Initialize random number generator
-            Random random = new Random();
-
-            // Player's initial position
-            int playerPosition = 0;
-
             // Roll the die using ((RANDOM))
             int dieResult = random.Next(1, 7);
 
-            // Check for options: No Play, Ladder, or Snake
-            int option = random.Next(1, 4);
+        // Check for options: No Play, Ladder, or Snake
+        int option = random.Next(1, 4);
 
             switch (option)
             {
@@ -32,12 +37,23 @@ class SnakeLadder
                 case 3:
                     // Snake: Player moves behind by the number of positions received in the die
                     playerPosition -= dieResult;
+
+                    // Restart from 0 if the player's position goes below 0
+                    if (playerPosition< 0)
+                        playerPosition = 0;
+
                     Console.WriteLine("Snake! Player moves behind by " + dieResult + " positions. New position: " + playerPosition);
                     break;
                 default:
                     Console.WriteLine("Invalid option");
                     break;
             }
+    }
+
+        Console.WriteLine("Player reached the winning position: " + winningPosition);
         }
+
+       
     }
 }
+
